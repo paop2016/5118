1749081c6234f9914b1a8bb142465bc6d97126b5
Tyson Wang <469083378@qq.com>
2017-07-19 11:04:16 +0800

    修改接口

--- ip_manager.py
+++ ip_manager.py
@@ -27,9 +27,9 @@ class IpManager():
         print '尝试切换ip'
         try:
             if 'MacBook'==computer:
-                r=requests.get('http://www.xdaili.cn/ipagent//privateProxy/getDynamicIP/DD20177189193qV6tMo/03df27b41ddb11e79ff07cd30abda612?returnType=2')
+                r=requests.get('http://www.xdaili.cn/ipagent//privateProxy/getDynamicIP/DD20177182243APZyNH/81068b56519511e79d9b7cd30abda612?returnType=2')
             elif 'wangchangtong' in computer:
-                r=requests.get('http://www.xdaili.cn/ipagent//privateProxy/getDynamicIP/DD20177189193qV6tMo/03df27b41ddb11e79ff07cd30abda612?returnType=2')
+                r=requests.get('http://www.xdaili.cn/ipagent//privateProxy/getDynamicIP/DD20177190599ryEAmZ/74a7d06c50c811e79d9b7cd30abda612?returnType=2')
 
             ipdata=json.loads(r.text)['RESULT']
             host = ipdata['wanIp']
